67d1d01b275d02a1b343445ee54482409d1e9df7
Sanjay <56962781+sanjaysunil34@users.noreply.github.com>
2020-10-01 14:40:50 +0530

    Update roots.c

--- roots.c
+++ roots.c
@@ -1,37 +1,31 @@
-#include <stdio.h> 
-#include <math.h> 
-void main() 
-{ 
- int a,b,c; 
- float d,r1,r2,r,rp,ip; 
- printf("Enter the values of a,b,c:"); 
- scanf("%d%d%d",&a,&b,&c); 
- if(a==0) 
- printf("invalid"); 
- else 
- { 
- d=b*b-4*a*c; 
- if(d>0) 
- { 
- printf("Roots are real and distinct\n"); 
- r1=((-b)+sqrt(d))/(2.0*a); 
- r2=((-b)-sqrt(d))/(2.0*a); 
- printf("Roots are %f,%f\n",r1,r2); 
- } 
- else if(d==0) 
- { 
- printf("Roots are real and equal\n"); 
- r=(-b)/(2.0*a); 
- printf("Roots are %f,%f\n",r,r); 
- } 
- else 
- { 
- printf("Roots are imaginary\n"); 
- rp=(-b)/(2.0*a); 
- ip=sqrt(-d)/(2.0*a);
- printf("Roots are %f +i%f and %f-i%f\n",rp,ip,rp,ip);  } 
-  
- } 
-  
-  
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+float a,b,c,r1,r2,d,r1_imag;
+printf("Enter the coefficients :");
+scanf("%g%g%g",&a,&b,&c);
+d=(b*b)-(4*a*c);
+if (a==0)
+printf("Not a quadratic equation .");
+else if(d>0)
+{
+r1=(-b+sqrt(d))/(2*a);
+r2=(-b+sqrt(d))/(2*a);
+printf("The roots are real and unequal . \n");
+printf("The roots are %g and %g \n",r1,r2);
+}
+else if(d==0)
+{
+r1=-b/(2*a);
+printf("The roots are real and equal . \n");
+printf("The root is %g . \n",r1);
+}
+else
+{
+printf("The roots are imaginary. \n");
+r1_imag=(4*a*c)-(b*b);
+printf("The roots are %g+%gi and %g-%gi . \n",-b/(2*a),sqrt(r1_imag)/(2*a),-
+b/(2*a),sqrt(r1_imag));
+}
 }
