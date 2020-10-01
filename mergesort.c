eacbc2ba553ea7130aee6f8be666641275466f5b
sh221bbst <56499191+sh221bbst@users.noreply.github.com>
2020-10-01 14:05:51 +0530

    Add files via upload

---
+++ mergesort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+  int a[25],b[25],c[50],ano,bno,i,j;
+  printf("Enter the no. of elements in the first array:");
+  scanf("%d",&ano);
+  for(i=0;i<ano;i++)
+    scanf("%d",&a[i]);
+  printf("Enter the no. of elements in the second array:");
+  scanf("%d",&bno);
+  for(i=0;i<bno;i++)
+    scanf("%d",&b[i]);
+  for(i=0,j=0;i<ano&&j<bno;)
+  {
+    if(a[i]<b[j])
+    {
+      c[i+j]=a[i];
+      i++;
+    }
+    else
+    {
+      c[i+j]=b[j];
+      j++;
+    }
+  }
+  for(;i<ano;i++)
+    c[i+j]=a[i];
+  for(;j<bno;j++)
+    c[i+j]=b[j];
+  printf("The merged array is:\n");
+  for(int q=0;q<i+j;q++)
+    printf("%d\t",c[q]);
+}
