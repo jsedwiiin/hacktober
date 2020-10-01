9d04943be28cb5218057c956cefe2ede83928170
sh221bbst <56499191+sh221bbst@users.noreply.github.com>
2020-10-01 15:02:38 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(int n,char *a[])
+{
+  float avg=0;
+  if(n==1)
+    printf("At least 1 value required.\n");
+  else
+  {
+    for(int i=1;i<n;i++)
+      avg+=atoi(a[i]);
+    avg=avg/(n-1);
+    printf("The average is %f\n",avg);
+  }
+}
